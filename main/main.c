1d813fcf7bf920d1d7f557011a0bb0c5fe2b2468
d51x <dimonich110@gmail.com>
2021-01-04 18:12:57 +0300

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,480 @@
+#include "main.h"
+
+
+static const char *TAG = "MAIN";
+
+static void debug_register_http_print_data();
+static void initialize_services(void *arg);
+
+httpd_handle_t http_server = NULL;
+
+void app_main(void)
+{
+    #ifdef CONFIG_DEBUG_UART1
+        enable_debug_uart1();
+        userlog("DEBUG UART1: %s\n", __func__);
+    #endif
+
+	//ESP_ERROR_CHECK(nvs_flash_init());
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    //if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    //  ESP_ERROR_CHECK(nvs_flash_erase());
+    //  ret = nvs_flash_init();
+    //}
+    ESP_ERROR_CHECK(ret);
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+	
+    // get app version
+    const esp_app_desc_t *app_desc = esp_ota_get_app_description();
+    strncpy(FW_VER, app_desc->version, 32);
+    strcpy(FW_VER, copy_str_from_str(FW_VER, "_"));
+    free((void *)app_desc);
+
+    // ========================================= MODULES initialization START
+    #ifdef CONFIG_COMPONENT_DEBUG
+        log_rtc_print_debug_str();
+        log_rtc_init_debug_str();
+    #endif
+
+    initialize_modules();
+    user_setup(NULL);
+
+    xTaskCreate(initialize_services, "init_srv", 2048, 1000, 9, NULL);
+
+    #ifdef CONFIG_SENSOR_MQTT
+        mqtt_subscriber_init( http_server );
+    #endif
+
+    while (true) 
+    {
+        static uint32_t sec = 0;
+        
+        #ifdef CONFIG_DEBUG_PRINT_TASK_INFO
+            if ( sec % 2 )    // каждые 2 сек
+                print_tasks_info();
+        #endif
+        
+        user_loop(sec);
+        sec++;
+
+        vTaskDelay(1000/ portTICK_RATE_MS);
+    }
+
+	
+}
+
+
+void initialize_modules()
+{
+    #ifdef CONFIG_SENSORS_GET
+    http_handlers_count++; 
+    sensors_init();
+    #endif
+
+    #ifdef CONFIG_SENSOR_PZEM004_T
+    pzem_init(0);
+    pzem_task_start(5);
+
+        http_handlers_count += 1;
+    #endif
+
+    #ifdef CONFIG_COMPONENT_RELAY
+    // TODO: load relay's data from nvs and apply
+    
+    #ifdef CONFIG_RELAY_CONFIG
+    relay_load_nvs();
+    #endif
+
+    //#define RELAY_LED_PIN 2
+    //relay_h = relay_create( "Led", RELAY_LED_PIN, RELAY_LEVEL_HIGH /*RELAY_LEVEL_LOW*/ /* RELAY_LEVEL_HIGH*/ , false);
+    //relay_write(relay_h,  RELAY_STATE_CLOSE);
+
+    // relay_red_h = relay_create( "Red", 15, RELAY_LEVEL_LOW /*RELAY_LEVEL_LOW*/ /* RELAY_LEVEL_HIGH*/ );
+    // relay_write(relay_red_h,  RELAY_STATE_CLOSE);
+
+    // relay_green_h = relay_create( "Green", 12, RELAY_LEVEL_LOW /*RELAY_LEVEL_LOW*/ /* RELAY_LEVEL_HIGH*/ );
+    // relay_write(relay_green_h,  RELAY_STATE_CLOSE);
+
+    // relay_blue_h = relay_create( "Blue", 13, RELAY_LEVEL_LOW /*RELAY_LEVEL_LOW*/ /* RELAY_LEVEL_HIGH*/ );
+    // relay_write(relay_blue_h,  RELAY_STATE_CLOSE);   
+
+        #ifdef CONFIG_RELAY_HTTP
+        http_handlers_count += RELAY_HANDLERS_COUNT;
+        #endif 
+    #endif
+
+    #ifdef CONFIG_COMPONENT_IR_RECV
+    ir_rx = irrcv_init();
+    if ( ir_rx != NULL ) {
+		//		                btn_id	  btn_code   user_ctx      callback fun
+        irrcv_add_button(ir_rx, 	 0, 0x00FF14EB, "button1", NULL /*ir_button1_press*/);
+        irrcv_add_button(ir_rx, 	 1, 0x00FF04FB, "button2", NULL /*ir_button2_press*/);   
+		
+		//Start IR Receiver to receive a code
+        irrcv_start( ir_rx );
+    } else {
+        ESP_LOGE(TAG, "failed to init ir receiver");
+    }    
+        #ifdef CONFIG_IR_RECV_HTTP
+        http_handlers_count += IR_RECV_HANDLERS_COUNT;
+        #endif
+    #endif
+
+    #ifdef CONFIG_COMPONENT_LCD2004
+        lcd2004_init();
+        lcd2004_test_task();
+        #ifdef CONFIG_COMPONENT_LCD2004_HTTP
+        http_handlers_count += LCD2004_HANDLERS_COUNT;
+        #endif
+    #endif
+
+    #ifdef CONFIG_COMPONENT_MCP23017
+        mcp23017_h = mcp23017_create(0x20 /*MCP23017_ADDR_DEFAULT*/ );
+        #ifdef CONFIG_MCP23017_HTTP
+        http_handlers_count += MCP23017_HANDLERS_COUNT;
+        #endif 
+        
+    #endif
+
+    #ifdef CONFIG_COMPONENT_PCF8574
+        pcf8574_h = pcf8574_create(0x3F /*PCF8574_ADDR_DEFAULT*/ );
+        pcf8574_test_task(pcf8574_h);
+    #endif      
+
+    #ifdef CONFIG_SENSOR_SHT21
+        sht21_init();
+        sht21_start(5);
+    #endif
+
+    #ifdef CONFIG_LED_CONTROLLER
+        // // ===== create led channels ============================
+        // TODO: get ledcontrol data from nvs
+        // если функция чтения данных из nvs вернула ошибку, то используем занчения по дуфолту
+        uint8_t led_ch_cnt = ledcontrol_init_channels(&ledc_channels);
+        if ( led_ch_cnt > 0)
+        {
+            // ===== create led controller ============================
+            ledc_h = ledcontrol_create(500, led_ch_cnt);
+            //ledc = (ledcontrol_t *)ledc_h;
+
+            // ==== register led channels to led controller =============
+            // TODO: перенсти функцию регистрации в init, в качестве параметра передать массив каналов
+            for (uint8_t i = 0; i < led_ch_cnt; i++)
+            {
+                ledc_h->register_channel(ledc_channels[i]);
+            }
+            
+            // ====== initialize led controller =======================
+            ledc_h->init();  
+
+            #ifdef CONFIG_LED_CONTROL_HTTP
+                http_handlers_count += LED_CONTROL_HANDLERS_COUNT;
+            #endif
+
+            #ifdef CONFIG_RGB_CONTROLLER
+                // === create and init RGB controller ================
+                // change group to hide channels from default LED Controller
+                ledcontrol_channel_set_group(&ledc_channels[CONFIG_RGB_RED_CHANNEL], 255);
+                ledcontrol_channel_set_group(&ledc_channels[CONFIG_RGB_GREEN_CHANNEL], 255);
+                ledcontrol_channel_set_group(&ledc_channels[CONFIG_RGB_BLUE_CHANNEL], 255);
+                
+                // change names of rgb channels from default "ChannelX"
+                ledcontrol_channel_set_name(&ledc_channels[CONFIG_RGB_RED_CHANNEL], "Red");
+                ledcontrol_channel_set_name(&ledc_channels[CONFIG_RGB_GREEN_CHANNEL], "Green");
+                ledcontrol_channel_set_name(&ledc_channels[CONFIG_RGB_BLUE_CHANNEL], "Blue");
+
+                rgb_ledc = rgbcontrol_init( ledc_h, 
+                                            &ledc_channels[CONFIG_RGB_RED_CHANNEL], 
+                                            &ledc_channels[CONFIG_RGB_GREEN_CHANNEL], 
+                                            &ledc_channels[CONFIG_RGB_BLUE_CHANNEL]
+                                        );
+
+                #ifdef CONFIG_RGB_CONTROLLER_HTTP
+                    http_handlers_count += RGB_CONTROL_HANDLERS_COUNT;
+                #endif 
+
+                #ifdef CONFIG_RGB_EFFECTS
+                    // ==== create and init rgb effects =======================
+                    // === setup effects to RGB controller  
+                    effects = effects_init(rgb_ledc, rgb_ledc->set_color_hsv);
+                    rgb_ledc->set_effects( effects );  
+                #endif
+            #endif
+            
+        }
+    #endif
+
+    #ifdef I2C_HANDLERS_COUNT
+    http_handlers_count += I2C_HANDLERS_COUNT;
+    #endif
+
+    #ifdef CONFIG_SENSOR_MQTT
+    http_handlers_count += MQTT_SUBSCRIBER_HANDLERS_COUNT;
+    #endif
+
+}
+
+void initialize_modules_mqtt()
+{
+    user_mqtt_init(NULL);
+
+    #ifdef CONFIG_COMPONENT_RELAY
+    relay_mqtt_init();
+    #endif
+
+    #ifdef CONFIG_COMPONENT_MCP23017
+    mcp23017_mqtt_init(mcp23017_h);
+    #endif
+
+    #ifdef CONFIG_SENSOR_SHT21
+    sht21_mqtt_init();
+    #endif     
+
+    #ifdef CONFIG_LED_CONTROLLER
+    ledcontrol_mqtt_init(ledc_h);
+        #ifdef CONFIG_RGB_EFFECTS
+        rgbcontrol_mqtt_init(rgb_ledc);
+        #endif
+    #endif
+
+    #ifdef CONFIG_SENSOR_PZEM004_T
+    pzem_mqtt_init();
+    #endif
+}
+
+#ifdef CONFIG_USER_WEB_PRINT
+static void _user_web_main(http_args_t *args)
+{
+    http_args_t *arg = (http_args_t *)args;
+    httpd_req_t *req = (httpd_req_t *)arg->req;
+
+    httpd_resp_sendstr_chunk(req, html_block_data_no_header_start);
+
+    user_web_main(req);
+
+    httpd_resp_sendstr_chunk(req, html_block_data_end);
+}
+#endif
+
+#ifdef CONFIG_USER_WEB_CONFIG
+static void _user_web_options(http_args_t *args)
+{
+    
+    http_args_t *arg = (http_args_t *)args;
+    httpd_req_t *req = (httpd_req_t *)arg->req;
+        
+    char *buf = malloc( strlen(html_block_data_header_start) + 20 );
+    sprintf(buf, html_block_data_header_start, "User options");
+    httpd_resp_sendstr_chunk(req, buf);
+    free(buf); 
+
+    user_web_options(req);
+
+    httpd_resp_sendstr_chunk(req, html_block_data_end);
+}
+#endif
+
+void initialize_modules_http(httpd_handle_t _server)
+{
+    #ifdef CONFIG_SENSORS_GET
+    sensors_http_init(_server);
+    #endif
+
+    debug_register_http_print_data();
+    wifi_http_init( _server );
+    ota_http_init( _server );
+
+    #ifdef CONFIG_RELAY_HTTP
+    relay_http_init( _server );
+    #endif
+
+    #ifdef CONFIG_IR_RECV_HTTP
+    irrcv_http_init( _server, ir_rx );
+    #endif
+
+    #ifdef CONFIG_COMPONENT_I2C
+    i2c_http_init( _server );
+    #endif
+
+    #ifdef CONFIG_COMPONENT_LCD2004_HTTP
+    lcd2004_http_init( _server );
+    #endif
+
+    // USER HTTP INIT
+    user_http_init(NULL);
+
+    #ifdef CONFIG_MCP23017_HTTP
+    //((mcp23017_t *) mcp23017_h)->http_buttons = 0b1010100000000000;
+    mcp23017_http_init(_server, mcp23017_h);
+    #endif
+
+    mqtt_http_init(_server);
+
+    #ifdef CONFIG_SENSOR_SHT21
+    sht21_http_init(_server);
+    #endif
+
+    #ifdef CONFIG_LED_CONTROL_HTTP
+    ledcontrol_http_init(http_server, ledc_h);
+
+        #ifdef CONFIG_RGB_CONTROLLER_HTTP
+            rgbcontrol_http_init(_server, rgb_ledc );
+        #endif
+    #endif   
+
+    #ifdef CONFIG_SENSOR_PZEM004_T_WEB
+        pzem_http_init(_server);
+    #endif
+
+    #if CONFIG_USER_WEB_PRINT || CONFIG_USER_WEB_CONFIG
+    http_args_t *p = calloc(1,sizeof(http_args_t));
+    #endif
+
+    #ifdef CONFIG_USER_WEB_PRINT
+    register_print_page_block( "user1", PAGES_URI[ PAGE_URI_ROOT], 0, _user_web_main, p, NULL, NULL  );       
+    #endif
+
+    #ifdef CONFIG_USER_WEB_CONFIG
+    register_print_page_block( "user2", PAGES_URI[ PAGE_URI_CONFIG], 0, _user_web_options, p, user_process_param, NULL  );       
+    #endif
+
+}
+
+static void initialize_services(void *arg)
+{
+    
+    
+
+    wifi_init();
+    if ( wifi_cfg->mode != WIFI_MODE_AP)
+    {
+        sntp_start();
+    } 
+    else 
+    {
+
+    }
+
+    webserver_init(&http_server);
+    mqtt_init();
+    if ( wifi_cfg->mode != WIFI_MODE_AP)
+    {
+        mqtt_start();
+        initialize_modules_mqtt();
+    } 
+    else 
+    {
+
+    }
+    
+    initialize_modules_http( http_server );
+
+    vTaskDelete(NULL);
+}
+
+static void main_debug_print(http_args_t *args)
+{
+    //ESP_LOGW(TAG, __func__ );
+    http_args_t *arg = (http_args_t *)args;
+    httpd_req_t *req = (httpd_req_t *)arg->req;
+
+    // esp_chip_info_t chip_info;
+    // esp_chip_info(&chip_info);
+
+    system_info_t *sys_info = malloc(sizeof(system_info_t));
+    get_system_info(sys_info);
+    char *mac = calloc(1,6);
+    wifi_get_mac(mac);
+    sys_info->chip_info.chip_id = get_chip_id((uint8_t *)mac);
+    free(mac);
+
+    char flash_size[10];
+    switch (sys_info->mem_info.flash_size_map)
+    {
+        case FLASH_SIZE_4M_MAP_256_256:
+            strcpy(flash_size, "512 Kb");
+            break;
+        
+        case FLASH_SIZE_2M:
+            strcpy(flash_size, "256 Kb");
+            break;
+                
+        case FLASH_SIZE_8M_MAP_512_512:
+            strcpy(flash_size, "1 MB");
+            break;
+                        
+        case FLASH_SIZE_16M_MAP_512_512:
+            strcpy(flash_size, "2 MB");
+            break;       
+
+        case FLASH_SIZE_32M_MAP_512_512:
+            strcpy(flash_size, "4 MB");
+            break;
+        
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            strcpy(flash_size, "2 MB");
+            break;
+                
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            strcpy(flash_size, "4 MB");
+            break;
+                        
+        case FLASH_SIZE_32M_MAP_2048_2048:
+            strcpy(flash_size, "4 MB");
+            break;                        
+            
+        case FLASH_SIZE_64M_MAP_1024_1024:
+            strcpy(flash_size, "8 MB");
+            break; 
+
+        case FLASH_SIZE_128M_MAP_1024_1024:
+            strcpy(flash_size, "16 MB");
+            break;
+        
+        default:
+            break;
+    }
+    httpd_resp_sendstr_chunk(req, "CHIP INFO<br>");
+    httpd_resp_sendstr_chunk_fmt(req, "Model: %s rev.%d<br>ChipID: %d<br>"
+        , (sys_info->chip_info.chip_model == 0) ? "esp8266" : "esp32"
+        , sys_info->chip_info.chip_revision
+        , sys_info->chip_info.chip_id        
+        );
+
+    httpd_resp_sendstr_chunk_fmt(req, 
+    "<br>Flash: %s<br>"
+    "SPI Flash Size: %s<br>"
+    "Compile Size: %d Mb<br>"
+    //, sys_info->chip_info.features
+    , sys_info->chip_info.features & CHIP_FEATURE_EMB_FLASH ? "Embedded" : "External"
+    , flash_size
+    , sys_info->mem_info.flash_size / 0x100000 // 1024*1024
+    );
+
+    httpd_resp_sendstr_chunk_fmt(req, "Free Heap: %d<br>", sys_info->mem_info.free_heap_size);
+    //httpd_resp_sendstr_chunk_fmt(req, "IDF Ver: %s<br>", sys_info->sdk_version);
+
+    free(sys_info);
+    
+    esp_reset_reason_t reason = esp_reset_reason();
+    httpd_resp_sendstr_chunk_fmt(req, "<br>Reset reason: %d (0x%02X) %s"
+                , reason, reason, RESET_REASONS[reason]);
+
+    #ifdef CONFIG_COMPONENT_DEBUG
+                httpd_resp_sendstr_chunk_fmt(req, "<br>Debug str last: %s", log_rtc_get_debug_str(0));
+                httpd_resp_sendstr_chunk_fmt(req, "<br>Debug str prev1: %s", log_rtc_get_debug_str(1));
+                httpd_resp_sendstr_chunk_fmt(req, "<br>Debug str prev2: %s", log_rtc_get_debug_str(2));
+                httpd_resp_sendstr_chunk_fmt(req, "<br>Debug str prev3: %s", log_rtc_get_debug_str(3));
+    #endif
+}
+
+void debug_register_http_print_data()
+{
+    http_args_t *p = calloc(1,sizeof(http_args_t));
+    register_print_page_block( "debug", PAGES_URI[ PAGE_URI_DEBUG], 0, main_debug_print, p, NULL, NULL  ); 
+}
---
+++ main/user.c
@@ -0,0 +1,373 @@
+#include "user.h"
+#include "button.h"
+
+static const char *TAG = "USER";
+
+#ifdef CONFIG_MCP23017_ISR
+void test_mcp23017_isr_cb1(char *buf)
+{
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 0, &val);
+    val = !val;
+        ESP_LOGI(TAG, "executed callback %s %d", __func__, val);
+    mcp23017_write_pin(mcp23017_h, 0, val);    
+}
+
+void test_mcp23017_isr_cb2(char *buf)
+{
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 1, &val);
+    val = !val;
+    ESP_LOGI(TAG, "executed callback %s %d", __func__, val);    
+    mcp23017_write_pin(mcp23017_h, 1, val);    
+}
+
+void test_mcp23017_isr_cb3(char *buf)
+{
+
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 2, &val);
+    val = !val;
+        ESP_LOGI(TAG, "executed callback %s %d", __func__, val);
+    mcp23017_write_pin(mcp23017_h, 2, val);    
+}
+
+void test_mcp23017_isr_cb4(char *buf)
+{
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 3, &val);
+    val = !val;
+    ESP_LOGI(TAG, "executed callback %s %d", __func__, val);    
+    mcp23017_write_pin(mcp23017_h, 3, val);    
+}
+
+void test_mcp23017_isr_cb5(char *buf)
+{
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 4, &val);
+    val = !val;
+        ESP_LOGI(TAG, "executed callback %s %d", __func__, val);
+    mcp23017_write_pin(mcp23017_h, 4, val);    
+}
+
+void test_mcp23017_isr_cb6(char *buf)
+{
+    static uint8_t val = 0;
+
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+    mcp23017_read_pin(mcp23017_h, 5, &val);
+    val = !val;
+    ESP_LOGI(TAG, "executed callback %s %d", __func__, val);
+    mcp23017_write_pin(mcp23017_h, 5, val);
+}
+
+void test_mcp23017_isr_cb7(char *buf)
+{
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 6, &val);
+    val = !val;
+    ESP_LOGI(TAG, "executed callback %s %d", __func__, val);
+    mcp23017_write_pin(mcp23017_h, 6, val);
+}
+
+void test_mcp23017_isr_cb8(char *buf)
+{
+    
+    mcp23017_handle_t mcp23017_h = (mcp23017_handle_t ) buf;
+
+    static uint8_t val = 0;
+    mcp23017_read_pin(mcp23017_h, 7, &val);
+    val = !val;
+    ESP_LOGI(TAG, "executed callback %s %d", __func__, val);
+    mcp23017_write_pin(mcp23017_h, 7, val);
+}
+
+#endif
+
+// void test1(char *buf, void *args);
+// void test2(char *buf, void *args);
+
+// void test_recv1(char *buf, void *args);
+// void test_recv2(char *buf, void *args);
+
+void pressed1_cb();
+void pressed2_cb();
+void pressed3_cb();
+void hold_2s_cb();
+void hold_3s_cb();
+void hold_10s_cb();
+
+void user_setup(void *args)
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+    
+    // if ( softuart_open(0, 9600, 0 /*RX*/, 2 /*TX*/) ) {
+    //     ESP_LOGW(TAG, "SoftUart0 opened" );
+    // } else {
+    //     ESP_LOGE(TAG, "SoftUart0 open ERROR" );
+    // }
+    //http_handlers_count++;
+
+    #ifdef CONFIG_COMPONENT_RELAY
+    #define RELAY_LED_PIN 2
+    relay_h = relay_create( "Led", RELAY_LED_PIN, RELAY_LEVEL_HIGH /*RELAY_LEVEL_LOW*/ /* RELAY_LEVEL_HIGH*/ , false);
+    relay_write(relay_h,  RELAY_STATE_CLOSE);    
+
+    //button_handle_t btn_g4_h = configure_push_button(GPIO_NUM_4, BUTTON_ACTIVE_HIGH);
+    button_handle_t btn_g4_h = configure_push_button(GPIO_NUM_4, BUTTON_ACTIVE_LOW);
+    if (btn_g4_h) 
+    {
+        // регистрируем коллбек короткого нажатия
+        #define MAX_SHORT_PRESSED_COUNT 3
+        button_cb *short_pressed_cb = calloc(MAX_SHORT_PRESSED_COUNT, sizeof(button_cb));
+        // заполним массив указателями на функции
+        short_pressed_cb[0] = &pressed1_cb;
+        short_pressed_cb[1] = NULL; //&pressed2_cb; // NULL если не требуется обрабатывать 2-ое (n-Раз) нажатие
+        short_pressed_cb[2] = &pressed3_cb;
+
+        // 1..3 коротких нажатий в течение 500 мсек
+        button_set_on_presscount_cb(btn_g4_h, 500, MAX_SHORT_PRESSED_COUNT, short_pressed_cb);
+
+        // сработает при отпускании после 1 сек не зависимо сколько держали по времени
+        #define BTN_HOLD_1_SEC 2
+        button_add_on_release_cb(btn_g4_h, BTN_HOLD_1_SEC, hold_2s_cb, NULL);
+        
+        // сработает при удержании более 2 сек
+        #define BTN_HOLD_2_SEC 3
+        button_add_on_press_cb(btn_g4_h, BTN_HOLD_2_SEC, hold_3s_cb, NULL);        
+
+        // сработает при удержании более 10 сек
+        #define BTN_HOLD_10_SEC 10
+        button_add_on_press_cb(btn_g4_h, BTN_HOLD_10_SEC, hold_10s_cb, NULL);
+    }
+    #endif
+}
+
+void user_http_init(void *args)
+{
+    // функция вызывается после user_setup и старта webserver'a
+    ESP_LOGW(TAG, LOG_FMT() );
+
+    #ifdef CONFIG_MCP23017_HTTP
+    mcp23017_http_set_btn_name(mcp23017_h, 15, "Кнопка 1");
+    mcp23017_http_set_btn_name(mcp23017_h, 13, "Кнопка 2");
+    mcp23017_http_set_btn_name(mcp23017_h, 11, "Кнопка 3");
+    #endif
+
+    #ifdef CONFIG_LED_CONTROL_HTTP
+    //ledcontrol_http_add_group(ledc_h, "RGB Controller", 1, 5);
+    ledcontrol_http_add_group(ledc_h, "Белая подсветка", 2, 6);
+    #endif    
+
+    #ifdef CONFIG_COMPONENT_MCP23017
+        //mcp23017_test_task(mcp23017_h);
+        #ifdef CONFIG_MCP23017_ISR
+        // 1 - сразу при нажатии
+        // 2 - только после отпускания
+        mcp23017_isr_handler_add(mcp23017_h, 15, 2, test_mcp23017_isr_cb8, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 14, 1, test_mcp23017_isr_cb7, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 13, 1, test_mcp23017_isr_cb6, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 12, 1, test_mcp23017_isr_cb5, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 11, 1, test_mcp23017_isr_cb4, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 10, 1, test_mcp23017_isr_cb3, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 9, 2, test_mcp23017_isr_cb2, mcp23017_h);
+        mcp23017_isr_handler_add(mcp23017_h, 8, 2, test_mcp23017_isr_cb1, mcp23017_h);
+        #endif
+    #endif
+}
+
+// функция вызывается после user_setup и старта mqtt
+// в этой функции можно зарегистрировать свои кастомные колбеки на отправку и получение данных
+void user_mqtt_init(void *args)
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+
+    //mqtt_add_periodic_publish_callback( "test1", test1, NULL);
+    //mqtt_add_periodic_publish_callback( "test2", test2, NULL);
+
+    //mqtt_add_receive_callback("recv1", 1, test_recv1, NULL);
+    //mqtt_add_receive_callback("recv2", 1, test_recv2, NULL);
+
+
+}
+
+// функция вызывает в основном цикле каждую секунду
+void user_loop(uint32_t sec)
+{
+    //int level = gpio_get_level(GPIO_NUM_4);
+    //ESP_LOGW( TAG, "gpio4 = %d", level);
+    // // Set Addr: B4 C0 A8 01 01 00 1E
+    // uint8_t cmd[7] = { 0xB4, 0xC0, 0xA8, 0x01, 0x01, 0x00, 0x1E};
+    
+    // //if ( softuart_puts(0, cmd)) 
+    // if ( softuart_write_bytes(0, cmd, 7)) 
+    // {
+    //     ESP_LOGW(TAG, "SoftUart0 softuart_puts OK" );
+    // } 
+    // else 
+    // {
+    //     ESP_LOGE(TAG, "SoftUart0 softuart_puts ERROR" );
+    // }
+    
+    // //pauseTask(50);
+    
+    // uint8_t readed = 0;
+    // char buf[7];
+    // readed = softuart_read_buf(0, buf, 7);
+
+
+    // if ( readed == 0 ) 
+    // {
+    //     ESP_LOGE(TAG, "SoftUart0 no data available" );
+    // } 
+    // else 
+    // {
+    //     ESP_LOGW(TAG, "SoftUart0 readed %d, buf: ", readed );
+    //     for ( uint8_t i=0; i < readed; i++)
+    //     {
+    //         ESP_LOGW(TAG, "0x%02X ", buf[i] );
+    //     }
+        
+    // }
+
+        #if CONFIG_SENSOR_SHT21 && CONFIG_COMPONENT_LCD2004
+
+            char s[20];
+            //char *s = (char *) calloc( 20 + 1, sizeof(char*));
+            //memset(s, 0, 20 + 1);
+
+            if ( xSemaphoreLCD2004 != NULL && xSemaphoreTake( xSemaphoreLCD2004, I2C_SEMAPHORE_WAIT ) == pdTRUE ) 
+            {
+                sprintf(s, "Tmp: %2.1f Hum: %2.1f", sht21_get_temp(), sht21_get_hum());
+                lcd2004_print(1, s );
+
+                //lcd2004_set_cursor_position( 1, 1);
+                //lcd2004_print_string( s );
+
+                //memset(s, 0, 20 + 1);
+                sprintf(s, "Freemem: %5d", esp_get_free_heap_size());
+                lcd2004_print(2, s );
+
+                //memset(s, 0, 20 + 1);
+                get_uptime(s);
+                lcd2004_print(3, s );
+                //lcd2004_set_cursor_position( 1, 2);
+                //lcd2004_print_string( s );
+                //free(s);
+                
+                xSemaphoreGive( xSemaphoreLCD2004 );
+            }           
+        #endif    
+}
+
+#ifdef CONFIG_USER_WEB_PRINT
+// функция вывод данные в пользовательском блоке на главной
+void user_web_main(httpd_req_t *req)
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+
+    //httpd_resp_sendstr_chunk(req, "Hello!");
+    //USER_WEB_PRINT("Hello User!");
+    //USER_WEB_PRINT("Hello User1!");
+    //USER_WEB_PRINT("Hello User2!");
+    //USER_WEB_PRINT("Hello User3!");
+
+    //char data[20];
+    //sprintf(data, "<br>Temp: %2.1f", sht21_get_temp());
+    //USER_WEB_PRINT(data);
+    //USER_WEB_PRINT("<br>");
+
+    //mcp23017_print_button(mcp23017_h, req, "mcp13", 13);
+    //relay_print_button(req, "btn1", 1);
+    //relay_print_button(req, "btn2", 2);
+    //relay_print_button(req, "btn3", 3);
+}
+#endif 
+
+#ifdef CONFIG_USER_WEB_CONFIG
+void user_web_options(httpd_req_t *req)
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+       
+    //USER_WEB_PRINT("Hello User Options!");
+    //USER_WEB_PRINT("Hello User1 Options!");
+    //USER_WEB_PRINT("Hello User2 Options!");
+    //USER_WEB_PRINT("Hello User3 Options!");
+
+}
+
+
+void user_process_param(httpd_req_t *req, void *args)
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+    //ESP_LOGW(TAG, "Hello User process param!");
+}
+#endif
+
+//mqtt_add_periodic_publish_callback( const char *topic, func_mqtt_send_cb fn_cb);
+// void mqtt_add_receive_callback( const char *topic, func_mqtt_recv_cb fn_cb); -
+/*
+void test1(char **buf, void *args) {
+    static uint32_t cnt = 0;
+    itoa(cnt++, *buf, 10);
+}
+
+void test2(char **buf, void *args){
+    static uint32_t cnt = 1000000;
+    sprintf(*buf, "%d", cnt);
+    cnt -= 10;
+    if ( cnt == 0) cnt = 1000000;
+}
+
+void test_recv1(char *buf, void *args)
+{
+    ESP_LOGI(TAG, "received topic 'recv1' with data: %s", buf);
+}
+
+void test_recv2(char *buf, void *args)
+{
+    ESP_LOGI(TAG, "received topic 'recv2' with data: %s", buf);
+}
+*/
+
+void IRAM_ATTR pressed1_cb()
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+}
+
+void IRAM_ATTR pressed2_cb()
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+}
+
+void IRAM_ATTR pressed3_cb()
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+}
+
+void hold_2s_cb()
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+}
+
+void hold_3s_cb()
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+}
+
+void hold_10s_cb()
+{
+    ESP_LOGW(TAG, LOG_FMT() );
+}
